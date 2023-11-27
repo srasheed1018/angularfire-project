7fd054e81995e6f05ce0d27b03b11f07998fa8ff
Siddeeq Rasheed <srasheed1018@gmail.com>
2023-11-27 16:10:34 -0500

    prettied up homepage

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
-  constructor(public afAuth: AngularFireAuth) {}
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver, public afAuth: AngularFireAuth) {}
 }
